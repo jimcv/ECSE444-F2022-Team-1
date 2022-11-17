d2a67e2ab101958286eeae311aa208dfa2b7d82a
Thomas <jung.h.song@mail.mcgill.ca>
2022-11-16 20:24:16 -0500

    Added Some basic functions

---
+++ final-project/Inc/game_engine.h
@@ -0,0 +1,13 @@
+/*
+ * game_engine.h
+ *
+ *  Created on: Nov 16, 2022
+ *      Author: Thomas Song
+ */
+
+#ifndef GAME_ENGINE_H_
+#define GAME_ENGINE_H_
+
+#include "game_engine.c"
+
+#endif /* GAME_ENGINE_H_ */
