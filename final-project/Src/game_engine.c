9a6c274853c8ccb0cbf53c78af6a60bbe432aed3
Thomas <jung.h.song@mail.mcgill.ca>
2022-11-16 20:24:16 -0500

    Added Some basic functions

--- final-project/Src/game_engine.c
+++ final-project/Src/game_engine.c
@@ -5,10 +5,17 @@
  *      Author: Thomas Song
  */
 
+/* Private Includes ------------------------ */
+#include <stdio.h>
+
 /* Private Defines ------------------------- */
+//enemy
 #define MAX_ENEMYHP 3
 #define MAX_NUM_ENEMIES 10
+//player
 #define MAX_PLAYERHP 5
+//projectile
+#define MAX_PROJECTILE 5
 
 /* Private Variable ------------------------ */
 enum entity { player, enemy };
@@ -22,10 +29,31 @@ typedef struct ENTITY {
 	int health;
 	enum entity type;
 }entity_t;
-/* Private Lists ------------------------- */
+
+typedef struct PROJECTILE {
+	int posit_x;
+	int posit_y;
+}projectile_t;
+
+/* Public entities ------------------------- */
 entity_t enemyList[MAX_NUM_ENEMIES];
+entity_t playerChar;
+projectile_t projectileList[MAX_PROJECTILE];
 
+//Player functions
+void createPlayer(){
+	entity_t thisPlayer;
+	thisPlayer.health = MAX_PLAYERHP;
+	thisPlayer.posit_x = 24;
+	thisPlayer.posit_y = 49;
+	thisPlayer.type = player;
+	playerChar = thisPlayer;
+}
 
+entity_t getPlayer(){
+	return playerChar;
+}
+//Enemy functions
 void createEnemies() {
 	for (int currEnemies = 0; currEnemies < MAX_NUM_ENEMIES; currEnemies++) {
 		entity_t thisEnemy;
@@ -35,5 +63,26 @@ void createEnemies() {
 		thisEnemy.posit_x = 19 + currEnemies;
 		thisEnemy.posit_y = 10;
 		enemyList[currEnemies] = thisEnemy;
+	}
+}
+
+entity_t* getEnemyList(){
+	return enemyList;
+}
+//Movement functions
+void moveRight(entity_t entity){
+	entity.posit_x++;
+}
+void moveLeft(entity_t entity){
+	entity.posit_x--;
+}
+//Collision detections
+void collisionDetection() {
+	for (int j = 0; j < MAX_PROJECTILE; j++) {
+		if(projectileList[j].posit_x == playerChar.posit_y && projectileList[j].posit_x == playerChar.posit_y){
+			if(playerChar.health > 0){
+				playerChar.health--;
+			}
+		}
 	}
 }
