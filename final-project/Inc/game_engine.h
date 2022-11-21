faa717df73e52425140b72018e52fc0f3d2f5093
Thomas <jung.h.song@mail.mcgill.ca>
2022-11-20 19:39:44 -0500

    resolved

--- final-project/Inc/game_engine.h
+++ final-project/Inc/game_engine.h
@@ -8,7 +8,6 @@
 #ifndef GAME_ENGINE_H_
 #define GAME_ENGINE_H_
 
-<<<<<<< HEAD
 #include "main.h"
 
 //functions
@@ -16,8 +15,5 @@ int updateGame(user* user_t,enemy* enemies_t, projectile* projectiles_t);
 void createPlayer(user* user_t);
 void createEnemies(enemy* enemy_t);
 void createProjectile(int x, int y,projectile* projectiles_t);
-=======
-#include "game_engine.c"
->>>>>>> c41b8e6330de329c44d57c3b86d7549564bc69d1
 
 #endif /* GAME_ENGINE_H_ */
