39eee2140dd4bc878b7e8dbfcdd9da8b72bbca1a
Jimmy Sheng <71361453+jimcv@users.noreply.github.com>
2022-12-01 12:20:24 -0500

    [ASM] use asm_feedback

--- final-project/Src/MadgwickAHRS.c
+++ final-project/Src/MadgwickAHRS.c
@@ -37,6 +37,7 @@ volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;	// quaternion of sens
 
 float invSqrt(float x);
 extern void asm_inv_sqrt(float *pIn, float *pOut);
+extern void asm_feedback(float *pIn, float *pOut);
 
 //====================================================================================================
 // Functions
@@ -151,9 +152,9 @@ void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float 
 void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az) {
 	float recipNorm;
 	float temp;
-	float s0, s1, s2, s3;
+//	float s0, s1, s2, s3;
 	float qDot1, qDot2, qDot3, qDot4;
-	float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
+//	float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
 
 	// Rate of change of quaternion from gyroscope
 	qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
@@ -164,45 +165,53 @@ void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, flo
 	// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
 	if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
 
-		// Normalise accelerometer measurement
-		temp = ax * ax + ay * ay + az * az;
-		asm_inv_sqrt(&temp, &recipNorm);
-		ax *= recipNorm;
-		ay *= recipNorm;
-		az *= recipNorm;
+		float input_array[11] = {ax, ay, az, gx, gy, gz, q0, q1, q2, q3, beta};
+		float output_array[4] = {qDot1, qDot2, qDot3, qDot4};
+		asm_feedback(input_array, output_array);
+		qDot1 = output_array[0];
+		qDot2 = output_array[1];
+		qDot3 = output_array[2];
+		qDot4 = output_array[3];
 
-		// Auxiliary variables to avoid repeated arithmetic
-		_2q0 = 2.0f * q0;
-		_2q1 = 2.0f * q1;
-		_2q2 = 2.0f * q2;
-		_2q3 = 2.0f * q3;
-		_4q0 = 4.0f * q0;
-		_4q1 = 4.0f * q1;
-		_4q2 = 4.0f * q2;
-		_8q1 = 8.0f * q1;
-		_8q2 = 8.0f * q2;
-		q0q0 = q0 * q0;
-		q1q1 = q1 * q1;
-		q2q2 = q2 * q2;
-		q3q3 = q3 * q3;
-
-		// Gradient decent algorithm corrective step
-		s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
-		s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
-		s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
-		s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
-		temp = s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3;
-		asm_inv_sqrt(&temp, &recipNorm); // normalise step magnitude
-		s0 *= recipNorm;
-		s1 *= recipNorm;
-		s2 *= recipNorm;
-		s3 *= recipNorm;
-
-		// Apply feedback step
-		qDot1 -= beta * s0;
-		qDot2 -= beta * s1;
-		qDot3 -= beta * s2;
-		qDot4 -= beta * s3;
+//		// Normalise accelerometer measurement
+//		temp = ax * ax + ay * ay + az * az;
+//		asm_inv_sqrt(&temp, &recipNorm);
+//		ax *= recipNorm;
+//		ay *= recipNorm;
+//		az *= recipNorm;
+//
+//		// Auxiliary variables to avoid repeated arithmetic
+//		_2q0 = 2.0f * q0;
+//		_2q1 = 2.0f * q1;
+//		_2q2 = 2.0f * q2;
+//		_2q3 = 2.0f * q3;
+//		_4q0 = 4.0f * q0;
+//		_4q1 = 4.0f * q1;
+//		_4q2 = 4.0f * q2;
+//		_8q1 = 8.0f * q1;
+//		_8q2 = 8.0f * q2;
+//		q0q0 = q0 * q0;
+//		q1q1 = q1 * q1;
+//		q2q2 = q2 * q2;
+//		q3q3 = q3 * q3;
+//
+//		// Gradient decent algorithm corrective step
+//		s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+//		s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+//		s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+//		s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
+//		temp = s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3;
+//		asm_inv_sqrt(&temp, &recipNorm); // normalise step magnitude
+//		s0 *= recipNorm;
+//		s1 *= recipNorm;
+//		s2 *= recipNorm;
+//		s3 *= recipNorm;
+//
+//		// Apply feedback step
+//		qDot1 -= beta * s0;
+//		qDot2 -= beta * s1;
+//		qDot3 -= beta * s2;
+//		qDot4 -= beta * s3;
 	}
 
 	// Integrate rate of change of quaternion to yield quaternion
